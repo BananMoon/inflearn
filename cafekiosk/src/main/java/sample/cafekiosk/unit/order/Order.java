82489e610259e101a76be4390608d5edacba15a7
BananMoon <younji1115@gmail.com>
2024-02-20 23:48:19 +0900

    feat: 키오스크 주문 등록/취소/가격 계산 기능 +추가 요구사항 개발

---
+++ cafekiosk/src/main/java/sample/cafekiosk/unit/order/Order.java
@@ -0,0 +1,12 @@
+package sample.cafekiosk.unit.order;
+
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.unit.beverage.Beverage;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@RequiredArgsConstructor
+public class Order {
+    private final LocalDateTime orderDateTime;
+    private final List<Beverage> orderedBeverages;
+}
